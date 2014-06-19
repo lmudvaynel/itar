3054064d4d1cfbd634a66e02e702a81a82240c4d
Artjom Juferov <derevjaginti@gmail.com>
2014-06-19 19:35:08 +0300

    deploy done

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ require 'capistrano/ext/multistage'
 set :application, "Itar" #write app name there
 
 set :scm, :git
-set :repository,  "git@github.com:derevjaginti/west-alko.git" #place your repo there
+set :repository,  "git@github.com:derevjaginti/itar.git" #place your repo there
 
 set :deploy_via, :remote_cache
 
